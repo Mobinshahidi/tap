a0984394aff2af799e78c3c172c3d3fdc3f4e4c3
mobinshahidi <mowbinsh@gmail.com>
2026-04-22 01:23:03 +0330

    fix: remove missing compose plugin and set compiler extension

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
     jacoco
 }
 
@@ -69,6 +68,10 @@ android {
 
     buildFeatures {
         compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
 
     packaging {
